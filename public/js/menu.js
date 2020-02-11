5b75527b52ea468686789ba64924bd8e37e98d3a
emja9213 <emanuel.jansson.9213@user.uu.se>
2020-02-11 16:43:24 +0100

    Task 4-9 completed.

---
+++ public/js/menu.js
@@ -0,0 +1,37 @@
+let food = [
+	{
+		"name": "Crispy No Chicken",
+		"kCal": 600,
+		"lactose": false,
+		"gluten": true,
+		"imagePath": "https://www.max.se/contentassets/ff1439a7a1624cf4a6b068c37d0db25f/product_crispy-no-chicken2.jpg"
+	},
+	{
+		"name": "BBQ Sandwich",
+		"kCal": 440,
+		"lactose": false,
+		"gluten": true,
+		"imagePath": "https://www.max.se/contentassets/10aac66bde484d38bbf6e8b37c0e6e36/product_bbq-sandwich2.jpg"
+	},
+	{
+		"name": "Crispy Nuggets",
+		"kCal": 470,
+		"lactose": false,
+		"gluten": false,
+		"imagePath": "https://www.max.se/contentassets/8c0ab650367341b988b9de30c99769f3/product_crispy-nuggets.jpg"
+	},
+	{
+		"name": "Delifresh Plant Beef",
+		"kCal": 870,
+		"lactose": true,
+		"gluten": true,
+		"imagePath": "https://www.max.se/contentassets/359fcb7f236247a7a4a41cdf2ddf36b0/product_delifresh-signature-plant-beef.jpg"
+	},
+	{
+		"name": "SmokeyChipotlePlantBeef",
+		"kCal": 920,
+		"lactose": true,
+		"gluten": true,
+		"imagePath": "https://www.max.se/contentassets/32f727872e644cf1803605d39bacd581/product_gdl-smokey-chipotle-plant-beef.jpg"
+	},
+]
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,34 +1,6 @@
-
-function menuItem(name, gluten, lactose, kCal, imagePath) {
-	this.name = name;
-	this.gluten = gluten;
-	this.lactose = lactose;
-	this.kCal = kCal;
-	this.imagePath = imagePath;
-
-	this.getName = function () {
-		return this.name;
-	}
-	this.getkCal = function () {
-		return this.kCal;
-	}
-}
-
-let crispyNoChicken = new menuItem("Crispy No Chicken", true, false, 600, "https://www.max.se/contentassets/ff1439a7a1624cf4a6b068c37d0db25f/product_crispy-no-chicken2.jpg");
-
-let bbqSandwich = new menuItem("BBQ Sandwich", true, false, 440, "https://www.max.se/contentassets/10aac66bde484d38bbf6e8b37c0e6e36/product_bbq-sandwich2.jpg");
-
-let crispyNuggets = new menuItem("Crispy Nuggets", false, false, 470, "https://www.max.se/contentassets/8c0ab650367341b988b9de30c99769f3/product_crispy-nuggets.jpg");
-
-let delifreshPlantBeef = new menuItem("Delifresh Plant Beef", true, true, 870, "https://www.max.se/contentassets/359fcb7f236247a7a4a41cdf2ddf36b0/product_delifresh-signature-plant-beef.jpg");
-
-let smokeyChipotlePlantBeef = new menuItem("Smokey Chipotle Plant Beef", true, true, 920, "https://www.max.se/contentassets/32f727872e644cf1803605d39bacd581/product_gdl-smokey-chipotle-plant-beef.jpg");
-
-let menu = [crispyNoChicken, bbqSandwich, crispyNuggets, delifreshPlantBeef, smokeyChipotlePlantBeef];
-
 const vueMenu = new Vue({
 	el: "#menuGrid",
 	data: {
-		menu: menu,
+		menu: food,
 	},
 })
