38de94957f441cd7c2a7ad43ea85a93c6299be6b
emja9213 <emanuel.jansson.9213@user.uu.se>
2020-02-12 01:59:57 +0100

    Halfway to 4-10.

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,3 +1,40 @@
+let button = document.getElementById("placeOrderButton");
+
+function functionClick() {
+	console.log("Button clicked!");
+}
+/*
+function submit() {
+	var fullName = document.getElementById("fullname").value;
+	var streetName = document.getElementById("street").value;
+	var houseNumber = document.getElementById("house").value;
+	var paymentList = document.getElementById("payment");
+	var paymentOption = paymentList.options[paymentList.selectedIndex].text;
+
+	var genders = document.getElementsByName('gender');
+	var selectedGender;
+	for (var i = 0; i < genders.length; i++) {
+		if (genders[i].checked) {
+			selectedGender = genders[i].value;
+			break;
+		}
+	}
+
+	console.log(fullName);
+	console.log(streetName);
+	console.log(houseNumber);
+	console.log(paymentOption);
+	console.log(selectedGender);
+
+	var array = [fullName, streetName, houseNumber, paymentOption, selectedGender];
+	return array;
+}
+*/
+/*
+button.addEventListener("click", submit)
+*/
+
+
 /*
 for (var i = 0; i < menu.length; i++) {
 	let burger = menu[i];
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,56 @@
+function submit() {
+	var fullName = document.getElementById("fullname").value;
+	var streetName = document.getElementById("street").value;
+	var houseNumber = document.getElementById("house").value;
+	var paymentList = document.getElementById("payment");
+	var paymentOption = paymentList.options[paymentList.selectedIndex].text;
+
+	var genders = document.getElementsByName('gender');
+	var selectedGender;
+	for (var i = 0; i < genders.length; i++) {
+		if (genders[i].checked) {
+			selectedGender = genders[i].value;
+			break;
+		}
+	}
+
+	console.log(fullName);
+	console.log(streetName);
+	console.log(houseNumber);
+	console.log(paymentOption);
+	console.log(selectedGender);
+
+	var array = [fullName, streetName, houseNumber, paymentOption, selectedGender];
+	return array;
+}
+
 const vueMenu = new Vue({
 	el: "#menuGrid",
 	data: {
-		menu: food,
+		menu: food
 	},
+});
+
+const vueDelivery = new Vue({
+	el: "#delivery",
+	data: {
+		fullname: "hej",
+		email: "fgsg",
+		streetname: "sfdg",
+		housenumber: "sdfg",
+		gender: "dsfg",
+		payment: "sdfg",
+		order: "sgf",
+	},
+	methods: {
+		markDone: function () {
+			console.log(fullname);
+			console.log(email);
+			console.log(streetname);
+			console.log(housenumber);
+			console.log(gender);
+			console.log(payment);
+			console.log(order);
+		}
+	}
 })
