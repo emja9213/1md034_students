8ed7fecf1f35e5acf0836b1c2d1f546d76415d38
emja9213 <Emanuel.Jansson.9213@student.uu.se>
2020-02-18 20:02:30 +0100

    Task 5-3 completed.

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -40,6 +40,14 @@ const vueDelivery = new Vue({
 	el: "#delivery",
 	data: {
 		orders: {},
+		localOrder: {
+			orderId: "T",
+			details: {
+				x: 0,
+				y: 0,
+			},
+			orderItems: [],
+		},
 		fullname: "",
 		email: "",
 		/*streetname: "",
@@ -48,36 +56,36 @@ const vueDelivery = new Vue({
 		payment: "Credit Card",
 		order: "",
 	},
-	created: function () {
-		/* When the page is loaded, get the current orders stored on the server.
-		 * (the server's code is in app.js) */
-		socket.on('initialize', function (data) {
-			this.orders = data.orders;
-		}.bind(this));
-
-		/* Whenever an addOrder is emitted by a client (every open map.html is
-		 * a client), the server responds with a currentQueue message (this is
-		 * defined in app.js). The message's data payload is the entire updated
-		 * order object. Here we define what the client should do with it.
-		 * Spoiler: We replace the current local order object with the new one. */
-		socket.on('currentQueue', function (data) {
-			this.orders = data.orders;
-		}.bind(this));
-	},
 	methods: {
 		markDone: function () {
-			this.order = '\n' + this.fullname + '\n' + this.email + '\n' + this.gender + '\n' + this.payment + '\n' + vueMenu.selectedBurgers
+			this.order = '\n' + this.fullname + '\n' + this.email + '\n' + this.gender + '\n' + this.payment + '\n' + vueMenu.selectedBurgers;
+			this.addOrder();
 		},
-    getNext: function () {
+		getNext: function () {
 			/* This function returns the next available key (order number) in
-			 * the orders object, it works under the assumptions that all keys
-			 * are integers. */
+			* the orders object, it works under the assumptions that all keys
+			* are integers. */
 			let lastOrder = Object.keys(this.orders).reduce(function (last, next) {
 				return Math.max(last, next);
 			}, 0);
 			return lastOrder + 1;
 		},
-		addOrder: function (event) {
+		addOrder: function () {
+			/* When you click in the map, a click event object is sent as parameter
+			 * to the function designated in v-on:click (i.e. this one).
+			 * The click event object contains among other things different
+			 * coordinates that we need when calculating where in the map the click
+			 * actually happened. */
+			socket.emit('addOrder', {
+				orderId: this.getNext(),
+				details: {
+					x: this.localOrder.details.x,
+					y: this.localOrder.details.y,
+				},
+				orderItems: vueMenu.selectedBurgers,
+			});
+		},
+		displayOrder: function (event) {
 			/* When you click in the map, a click event object is sent as parameter
 			 * to the function designated in v-on:click (i.e. this one).
 			 * The click event object contains among other things different
@@ -87,14 +95,8 @@ const vueDelivery = new Vue({
 				x: event.currentTarget.getBoundingClientRect().left,
 				y: event.currentTarget.getBoundingClientRect().top,
 			};
-			socket.emit('addOrder', {
-				orderId: this.getNext(),
-				details: {
-					x: event.clientX - 10 - offset.x,
-					y: event.clientY - 10 - offset.y,
-				},
-				orderItems: ['Beans', 'Curry'],
-			});
-		},
+			this.localOrder.details.x = event.clientX - 10 - offset.x;
+			this.localOrder.details.y = event.clientY - 10 - offset.y;
+		}
 	}
 })
